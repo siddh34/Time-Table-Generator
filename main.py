f443d5bc2e7231904ff32d79061f33c8e856fb05
siddh34 <ssiddharthsutar@gmail.com>
2022-11-22 15:47:44 +0530

    "Added "

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-
 # functions and classes
+def checkConstraintsForTY():
+    pass
 
+def checkConstraintsForSY():
+    pass
+
+def checkConstraints():
+    checkConstraintsForTY()
+    checkConstraintsForSY()
+    pass
+
+class subjects:
+    subForSY = []
+    subForTY = []
+    teachers = []
+    pass
 
 if __name__ == '__main__':
     pass
